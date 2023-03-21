f580d63ee1ac70a240a1ee6234b753ad5ff5ffaf
Kevin Cioch <kevin.cioch@gmail.com>
2023-03-21 15:45:49 +0100

    Added new movie model information to movie details page; Added release information to MovieListItem; Removed comment

--- frontend/src/components/programplan/MovieListItem.js
+++ frontend/src/components/programplan/MovieListItem.js
@@ -3,6 +3,7 @@ import Card from "react-bootstrap/Card";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
+import Badge from "react-bootstrap/Badge";
 
 const MovieListItem = (props) => {
   const { movie } = props;
@@ -11,17 +12,21 @@ const MovieListItem = (props) => {
     <Card style={{ width: "100%" }}>
       <Card.Body className={styles.movieListItem}>
         <div className={styles.cover}>
-          <Image
-            src={movie.media.images.poster}
-            fluid={true}
-          />
+          <Image src={movie.media.images.poster} fluid={true} />
         </div>
         <div className={styles.information}>
           <Card.Title>{movie.title}</Card.Title>
+          <p>
+            <Badge>{movie.release.ageRestriction}</Badge> •{" "}
+            {movie.release.productionYear} ({movie.release.productionCountry}) •{" "}
+            {movie.genres.join(", ")} • {movie.runtime}min
+          </p>
           <Card.Subtitle className="mb-2 text-muted">
             {movie.subtitle}
           </Card.Subtitle>
-          <Card.Text className={styles.description}>{movie.description}</Card.Text>
+          <Card.Text className={styles.description}>
+            {movie.description}
+          </Card.Text>
           <Link to={`/movies/${movie.title}`}>
             <Button>Details</Button>
           </Link>
--- frontend/src/pages/Movie.js
+++ frontend/src/pages/Movie.js
@@ -4,14 +4,17 @@ import Container from "../ui/Container";
 import Content from "../ui/Content";
 import Image from "react-bootstrap/Image";
 import styles from "./Movie.module.css";
-import {BACKEND_URL} from "../constants";
+import { BACKEND_URL } from "../constants";
+import HorizontalScrollContainer from "../ui/HorizontalScrollContainer";
+import CastAvatar from "../components/movies/forms/CastAvatar";
+import Badge from "react-bootstrap/Badge";
 
 const Movie = (props) => {
   const { id } = useParams();
   const [movie, setMovie] = useState();
 
   useEffect(() => {
-    console.log(BACKEND_URL)
+    console.log(BACKEND_URL);
     fetch(`${BACKEND_URL}/movies/${id}`)
       .then((res) => res.json())
       .then((data) => {
@@ -20,19 +23,69 @@ const Movie = (props) => {
       });
   }, []);
 
+  const castElements = movie?.credits?.cast?.map((el, i) => (
+    <CastAvatar key={i} person={el} />
+  ));
+  const keywordElements = movie?.keywords?.map((el, i) => (
+    <Badge key={i} bg="secondary">
+      {el}
+    </Badge>
+  ));
+
+  const genreElements = movie?.genres?.map((el, i) => (
+    <span key={i}>{el}</span>
+  ));
+
   return (
     <Container>
       {movie && (
         <Content>
-          <div className={styles.headerInfo}>
-            <Image src={movie.media.images.poster}  className={styles.poster} />
+          <section>
+            <div className={styles.headerInfo}>
+              <Image
+                src={movie.media.images.poster}
+                className={styles.poster}
+              />
 
-            <div>
-              <h1>{movie.title}</h1>
-              <h3 className="text-muted">{movie.subtitle}</h3>
-              <p className={styles.description}>{movie.description}</p>
+              <div>
+                <h1>{movie.title}</h1>
+                <p>
+                  <Badge>{movie.release.ageRestriction}</Badge> •{" "}
+                  {movie.release.productionYear} (
+                  {movie.release.productionCountry}) • {movie.genres.join(", ")}{" "}
+                  • {movie.runtime}min
+                </p>
+                <h3 className="text-muted">{movie.subtitle}</h3>
+                <p className={styles.description}>{movie.description}</p>
+                <div className="d-flex gap-5">
+                  <div>
+                    <h4 className="mb-1">Regie</h4>
+                    <p>{movie.credits.crew.director.name}</p>
+                  </div>
+                  <div>
+                    <h4 className="mb-1">Drehbuch</h4>
+                    <p>{movie.credits.crew.screenwriter.name}</p>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
+          </section>
+          <section className={styles.details}>
+            <div className={styles.cast}>
+              <h4>Cast</h4>
+              <HorizontalScrollContainer>
+                {castElements}
+              </HorizontalScrollContainer>
+            </div>
+            <div className={styles.sideInformation}>
+              <h4>Informationen</h4>
+              <p>
+                <b>Originaltitel:</b> {movie.originalTitle}
+              </p>
+              <h4>Schlüsselwörter</h4>
+              <div className="d-flex gap-1">{keywordElements}</div>
+            </div>
+          </section>
         </Content>
       )}
     </Container>
