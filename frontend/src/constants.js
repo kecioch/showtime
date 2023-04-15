9f96354b0136379695d70ace26aa8ed7df2451a4
Kevin Cioch <kevin.cioch@gmail.com>
2023-04-15 02:34:58 +0200

    Weekdays constants

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,5 @@
- const BACKEND_URL="http://localhost:4000"
+ const BACKEND_URL="http://localhost:4000";
 
-export { BACKEND_URL }
+ const DAYS_OF_WEEK=["Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+export { BACKEND_URL, DAYS_OF_WEEK }
