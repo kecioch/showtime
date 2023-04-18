7ad8448e112ee9d870fd38832c69874fbdc6fcca
Kevin Cioch <kevin.cioch@gmail.com>
2023-04-18 19:49:01 +0200

    Fixed bug weekdays

--- backend/services/Weekdays.js
+++ backend/services/Weekdays.js
@@ -14,7 +14,7 @@ const indexToWeekDay = (index) => weekdays[index];
 
 const getNextDate = (dayIndex) => {
   const now = new Date();
-  const daysUntilNextDayOfWeek = (dayIndex - now.getDay() + 7) % 7 + 1;
+  const daysUntilNextDayOfWeek = (dayIndex - now.getDay() + 7) % 7;
   const nextDate = new Date(
     now.getFullYear(),
     now.getMonth(),
