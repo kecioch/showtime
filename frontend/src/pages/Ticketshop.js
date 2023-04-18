5a8b551513fe4596eadd96effafc9329a9e185b2
Kevin Cioch <kevin.cioch@gmail.com>
2023-04-18 20:30:56 +0200

    Small adjustments for ticketshop

--- frontend/src/pages/Ticketshop.js
+++ frontend/src/pages/Ticketshop.js
@@ -6,12 +6,14 @@ import { useNavigate, useParams } from "react-router-dom";
 import Badge from "react-bootstrap/esm/Badge";
 import Image from "react-bootstrap/esm/Image";
 import styles from "./Ticketshop.module.css";
-import { getDateString, getTimeString } from "../services/FormatDate";
+import { getTimeString } from "../services/FormatDate";
+import SeatMap from "../components/seatmap/SeatMap";
 
 const Ticketshop = (props) => {
   const { id } = useParams();
   const [screening, setScreening] = useState();
   const navigate = useNavigate();
+  const [seatMap, setSeatMap] = useState();
 
   useEffect(() => {
     console.log("REQ ID", id);
@@ -23,9 +25,15 @@ const Ticketshop = (props) => {
       .then((data) => {
         console.log(data);
         setScreening(data);
+        setSeatMap(data.scheduledScreening.cinema);
+        console.log("MAP", data.scheduledScreening.cinema);
       })
       .catch((err) => navigate("/"));
   }, []);
+
+  const onSeatClickHandler = (seat) => {
+    console.log("SEATCLICKED", seat);
+  };
 
   return (
     <>
@@ -64,11 +72,12 @@ const Ticketshop = (props) => {
                   <h2 className="mt-5">
                     {screening.scheduledScreening.cinema.title}
                   </h2>
-                  <h3>
+                  <h4 className="mt-3">{new Date(screening.date).toLocaleString('de-de', {weekday:'long'})}</h4>
+                  <h4>
                     {new Date(screening.date).toLocaleDateString()},{" "}
                     {getTimeString(new Date(screening.scheduledScreening.time))}{" "}
                     Uhr
-                  </h3>
+                  </h4>
                 </div>
               </div>
             </section>
@@ -78,6 +87,17 @@ const Ticketshop = (props) => {
       <Container>
         <Content>
           <h2>Ticket Auswahl</h2>
+          <div className="d-flex">
+            <SeatMap
+              data={seatMap}
+              onSeatClick={onSeatClickHandler}
+              editMode={false}
+            />
+            <section>
+              <h3>Ausgewählte Tickets</h3>
+              <p>asd</p>
+            </section>
+          </div>
         </Content>
       </Container>
     </>
