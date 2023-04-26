6d5b3666af586b53d6d13ec5a5e3b32522fd3cc6
Kevin Cioch <kevin.cioch@gmail.com>
2023-04-26 22:55:33 +0200

    Fixed wrong datetime

--- backend/services/Datetime.js
+++ backend/services/Datetime.js
@@ -1,19 +1,20 @@
 const createDatetime = (date, time) => {
-  //   return new Date(
-  //     date.getFullYear(),
-  //     date.getMonth(),
-  //     date.getDate(),
-  //     time.getHours(),
-  //     time.getMinutes(),
-  //     time.getSeconds(),
-  //     time.getMilliseconds()
-  //   );
-  const datePart = date.toISOString().slice(0, 10);
-  const timePart = time.toISOString().slice(11, 23); 
+    return new Date(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate(),
+      time.getHours(),
+      time.getMinutes(),
+      time.getSeconds(),
+      time.getMilliseconds()
+    );
 
-  const combinedIsoString = datePart + "T" + timePart + "Z";
-  console.log("ISOSTRING", combinedIsoString);
-  return new Date(combinedIsoString);
+  // const datePart = date.toISOString().slice(0, 10);
+  // const timePart = time.toISOString().slice(11, 23); 
+
+  // const combinedIsoString = datePart + "T" + timePart + "Z";
+  // console.log("ISOSTRING", combinedIsoString);
+  // return new Date(combinedIsoString);
 };
 
 module.exports = { createDatetime };
