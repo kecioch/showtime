bc3d153fa5e4a75ed2b4aef4f27194f8040cf7ea
Kevin Cioch <kevin.cioch@gmail.com>
2023-05-03 02:18:30 +0200

    Added color for seat types & changed seat styling

--- backend/models/SeatType.js
+++ backend/models/SeatType.js
@@ -1,15 +1,25 @@
 const mongoose = require("mongoose");
 
 const seatTypeSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        unique: true,
-        required: true
+  title: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  colorHEX: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid hex color code!`,
     },
-    price: {
-        type: Number,
-        required: true
-    }
+  },
 });
 
 module.exports = mongoose.model("SeatType", seatTypeSchema);
--- frontend/src/components/seatmap/Seat.js
+++ frontend/src/components/seatmap/Seat.js
@@ -9,11 +9,16 @@ const Seat = (props) => {
 
   // let status = type === "empty" && !props.editMode ? "blank" : type;
 
+  console.log("SEAT", data);
+
   const type = !data.type ? "empty" : "standard";
+
   let status = type;
   if (type === "empty" && !props.editMode) status = "blank";
   if (data.status !== "unselected") status = data.status;
   const classes = `${styles.seat} ${styles[status]}`;
+
+  const bgColor = data?.type?.colorHEX && data.status !== "booked" ? data.type.colorHEX : "";
 
   const onMouseEnterHandler = (ev) => {
     if (ev.buttons !== 0) props.onSeatClick(data);
@@ -24,6 +29,7 @@ const Seat = (props) => {
       className={classes}
       onMouseEnter={onMouseEnterHandler}
       onClick={() => props.onSeatClick(data)}
+      style={{ backgroundColor: bgColor }}
     ></div>
   );
 };
