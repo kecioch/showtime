db3df1300885541c5dd3cd9bd2a7ec46051ab8b4
Kevin Cioch <kevin.cioch@gmail.com>
2023-05-18 15:56:53 +0200

    Changed toLocalString

--- backend/services/Tickets.js
+++ backend/services/Tickets.js
@@ -46,7 +46,7 @@ const sendTicket = async (ticket) => {
     const idHTML = `<h2>ID: ${id}</h2>`;
     const movieHTML = `<h3>${movie.title}</h2>`;
     const cinemaHTML = `<h3>${cinema.title}</h2>`;
-    const datetimeHTML = `<h2>${datetime.toLocaleString("de-de")}</h2>`;
+    const datetimeHTML = `<h2>${datetime.toLocaleString()}</h2>`;
     let seatsHTML = "";
     seats.map(
       (seat) =>
