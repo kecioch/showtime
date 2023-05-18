3819b651cfa14495637e8d2e2b49112b00a12934
Kevin Cioch <kevin.cioch@gmail.com>
2023-05-18 21:29:30 +0200

    Adjusted Datetime

--- backend/services/Datetime.js
+++ backend/services/Datetime.js
@@ -1,18 +1,34 @@
+const timeZone = {
+  offset: 60, // Standard offset from UTC in minutes (e.g., UTC+1)
+  dstOffset: 1, // DST offset in hours (e.g., 1 hour during DST)
+};
+
 const createDatetime = (date, time) => {
-    const datetime = new Date(date);
-    datetime.setUTCHours(time.getUTCHours());
-    datetime.setUTCMinutes(time.getUTCMinutes());
-    datetime.setUTCSeconds(time.getUTCSeconds());
-    datetime.setUTCMilliseconds(time.getUTCMilliseconds());
+  // const datetime = new Date(date);
+  // datetime.setUTCHours(time.getUTCHours());
+  // datetime.setUTCMinutes(time.getUTCMinutes());
+  // datetime.setUTCSeconds(time.getUTCSeconds());
+  // datetime.setUTCMilliseconds(time.getUTCMilliseconds());
+  // return datetime;
 
-    return datetime;
+  const datetime = new Date(date);
+  const originalTimeString = time.toLocaleString("en-US", {
+    timeZone: timeZone,
+    hour12: false,
+  });
 
-  // const datePart = date.toISOString().slice(0, 10);
-  // const timePart = time.toISOString().slice(11, 23); 
+  const originalTime = new Date(`2000-01-01T${originalTimeString}.000`);
+  const originalOffset = originalTime.getTimezoneOffset();
 
-  // const combinedIsoString = datePart + "T" + timePart + "Z";
-  // console.log("ISOSTRING", combinedIsoString);
-  // return new Date(combinedIsoString);
+  const dstOffset = timeZone.dstOffset * 60; // Convert DST offset from hours to minutes
+  const adjustedOffset = originalOffset + dstOffset;
+
+  datetime.setHours(time.getHours() + Math.floor(adjustedOffset / 60));
+  datetime.setMinutes(time.getMinutes() + (adjustedOffset % 60));
+  datetime.setSeconds(time.getSeconds());
+  datetime.setMilliseconds(time.getMilliseconds());
+
+  return datetime;
 };
 
 module.exports = { createDatetime };
