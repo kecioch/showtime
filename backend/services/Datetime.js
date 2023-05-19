2a58054073a388018b23c2f39ea48606405ce9e9
Kevin Cioch <kevin.cioch@gmail.com>
2023-05-19 02:32:32 +0200

    Added luxon timeZone

--- backend/services/Datetime.js
+++ backend/services/Datetime.js
@@ -1,39 +1,14 @@
-// const { DateTime } = require("luxon");
-const moment = require("moment");
-// const timeZone = "Europe/Berlin";
+const { DateTime } = require("luxon");
+// const moment = require("moment");
+const timeZone = "Europe/Berlin";
 
 const createDatetime = (date, time) => {
-  // const datetime = new Date(date);
-  // datetime.setUTCHours(time.getUTCHours());
-  // datetime.setUTCMinutes(time.getUTCMinutes());
-  // datetime.setUTCSeconds(time.getUTCSeconds());
-  // datetime.setUTCMilliseconds(time.getUTCMilliseconds());
-  // return datetime;
-
-  // const datetime = DateTime.fromJSDate(date, { zone: timeZone });
-  // const adjustedDatetime = datetime.set({
-  //   hour: time.getUTCHours(),
-  //   minute: time.getUTCMinutes(),
-  //   second: time.getUTCSeconds(),
-  //   millisecond: time.getUTCMilliseconds(),
-  // });
-
-  // return adjustedDatetime.toJSDate();
-
-  // const datetime = moment();
-  // datetime.set("year", date.getFullYear());
-  // datetime.set("month", date.getMonth());
-  // datetime.set("date", date.getDate());
-  // datetime.set("hour", time.getHours());
-  // datetime.set("minute", time.getMinutes());
-  // datetime.set("second", time.getSeconds());
-  // return datetime.toDate();
-
   const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const datetime = new Date(`${year}-${month}-${day}T${time}`);
-  return datetime;
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const datetimeString = `${year}-${month}-${day}T${time}`;
+  const datetime = DateTime.fromISO(datetimeString, { zone: timeZone });
+  return datetime.toJSDate();
 };
 
 module.exports = { createDatetime };
