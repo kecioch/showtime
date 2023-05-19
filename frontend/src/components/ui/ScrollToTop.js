c331901998abe8c4efe359805c74f1d01aecc49c
Kevin Cioch <kevin.cioch@gmail.com>
2023-05-19 15:23:34 +0200

    Changed ScrollToTop

--- frontend/src/components/ui/ScrollToTop.js
+++ frontend/src/components/ui/ScrollToTop.js
@@ -6,6 +6,11 @@ const ScrollToTop = () => {
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    document.body.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant",
+    });
   }, [pathname]);
 };
 
